93958d61456d901d377da1594036111218630806
ganeshnikam29 <ganeshnikam29@gmail.com>
2021-11-06 15:56:20 +0530

    cleaned the result reducer file

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,6 +4,14 @@ import { updatedObject } from '../utility';
 const initialState = {
   results: [],
 };
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElId
+  );
+  return updatedObject(state, { results: updatedArray });
+};
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
@@ -11,10 +19,7 @@ const resultReducer = (state = initialState, action) => {
         results: state.results.concat({ id: new Date(), val: action.result }),
       });
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElId
-      );
-      return updatedObject(state, { results: updatedArray });
+      return deleteResult(state, action);
     default:
       return state;
   }
